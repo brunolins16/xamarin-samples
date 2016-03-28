1740a7c6c96fff89dc09fe167b22cf309dcb3ec9
Bruno Oliveira (PFE) <brolivei@microsoft.com>
2016-03-28 20:44:11 -0300

    .

---
+++ Xamarin.CodeSharing/Xamarin.CodeSharing.CrossUI/Xamarin.CodeSharing.CrossUI/Page1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin.CodeSharing.CrossUI
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+            };
+        }
+    }
+}
